dde41833deda7630544a72a45b652e00afb315fd
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-27 14:16:15 +0530

    Create components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import {
+  CustomerReview,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "@/components";
+
 export default function Home() {
   return (
     <>
-      <p className="text-3xl">hii</p>
+      <main className="relative">
+        nav component
+        <section className="xl:p-1 wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+          <Services />
+        </section>
+        <section className="padding">
+          <SpecialOffer />
+        </section>
+        <section className="padding bg-pale-blue">
+          <CustomerReview />
+        </section>
+        <section className="padding-x sm:py-32 py-16w w-full">
+          <Subscribe />
+        </section>
+        <section className="padding-x padding-t pb-8 bg-black text-slate-300">
+          <Footer />
+        </section>
+      </main>
     </>
   );
 }
