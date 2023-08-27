9bf3b45235445d02c96c64f1c5831023adf75f62
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-27 22:14:36 +0530

    Build PopularProduct component

--- components/PopularProducts.tsx
+++ components/PopularProducts.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { products } from "@/constants";
+import { PopularProductCard } from ".";
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
-}
+    <section id="product" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-bold font-palanquin">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="font-montserrat lg:max-w-lg mt-12 text-slate-gray">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value.
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard
+            key={product.name}
+            imgURL={product.imgURL}
+            price={product.price}
+            name={product.name}
+            rating={product.rating}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default PopularProducts
+export default PopularProducts;
