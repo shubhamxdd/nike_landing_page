4d8e3e5c70a2e609a20bcbaa6f56157ee4444eb8
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-28 21:57:25 +0530

    Build CustomerReview component

--- components/CustomerReview.tsx
+++ components/CustomerReview.tsx
@@ -1,7 +1,29 @@
+import { reviews } from "@/constants";
+import { ReviewCard } from ".";
+
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red">Customers</span> Say?
+      </h3>
+      <p className="info-text text-center m-auto mt-4 max-w-lg">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            customerName={review.customerName}
+            feedback={review.feedback}
+            imgURL={review.imgURL}
+            rating={review.rating}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CustomerReview
+export default CustomerReview;
