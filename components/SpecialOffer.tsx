1636444cf564caf7fc7a012c41049eaa323aa105
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-28 21:33:37 +0530

    Build SpecialOffer component

--- components/SpecialOffer.tsx
+++ components/SpecialOffer.tsx
@@ -1,7 +1,53 @@
+import Image from "next/image";
+import { Button } from ".";
+import { offer } from "@/public/images";
+import { arrowRight } from "@/public/icons";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <>
+      <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+        <div className="flex-1">
+          <Image
+            src={offer}
+            width={773}
+            height={687}
+            alt="offer image"
+            className="object-contain w-full"
+          />
+        </div>
+        <div>
+          <h2 className="mt-10 font-palanquin text-4xl lg:max-w-lg font-bold capitalize">
+            <span className="text-coral-red">Special</span> Offer
+          </h2>
+          <p className="mt-4 lg:max-w-lg info-text">
+            Embark on a shopping journey that redefines your experience with
+            unbeatable deals. From premier selections to incredible savings, we
+            offer unparalleled value that sets us apart.
+          </p>
+          <p className="mt-6 lg:max-w-lg info-text">
+            Navigate a realm of possibilities designed to fulfill your unique
+            desires, surpassing the loftiest expectations. Your journey with us
+            is nothing short of exceptional.
+          </p>
+          <div className="mt-11 flex flex-wrap gap-4">
+            <Button
+              label="Shop Now"
+              iconURL={arrowRight}
+              hoverClass="hover:bg-white hover:text-slate-gray"
+            />
+            <Button
+              label="Learn More"
+              backgroundColor="bg-white"
+              textColor="text-slate-gray"
+              hoverClass="hover:bg-coral-red hover:text-white"
+              borderColor="border-coral-red"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
