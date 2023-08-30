90ff832ecefce225e74b71a96c48d21021ba4b6f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-30 11:10:40 +0530

    FInal

--- components/ShoeCardModal.tsx
+++ components/ShoeCardModal.tsx
@@ -57,13 +57,13 @@ const ShoeCardModal = ({
                   <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900"
+                      className="text-4xl font-medium text-gray-900"
                     >
                       {name}
                     </Dialog.Title>
-                    <Image src={imgURL} alt="shoe image" />
+                    <Image src={imgURL} alt="shoe image" className="mt-5" />
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">{price}</p>
+                      <p className="text-xl text-coral-red mt-5 mb-5">{price}</p>
                     </div>
 
                     <div className="mt-4">
@@ -72,7 +72,7 @@ const ShoeCardModal = ({
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                         onClick={closeModal}
                       >
-                        Got it, thanks!
+                        Close
                       </button>
                     </div>
                   </Dialog.Panel>
