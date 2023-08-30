3c7aab3aeb03fd9d12a5168fccad242c98995c4f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-30 10:43:37 +0530

    Create Initial modal component

--- components/PopularProducts.tsx
+++ components/PopularProducts.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { products } from "@/constants";
+
 import { PopularProductCard } from ".";
+import { useState } from "react";
+import ShoeCardModal from "./ShoeCardModal";
 
 const PopularProducts = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   return (
     <section id="products" className="max-container">
       <div className="flex flex-col justify-start gap-5">
@@ -15,13 +24,29 @@ const PopularProducts = () => {
       </div>
       <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
         {products.map((product) => (
-          <PopularProductCard
-            key={product.name}
-            imgURL={product.imgURL}
-            price={product.price}
-            name={product.name}
-            rating={product.rating}
-          />
+          <>
+            <div onClick={openModal}>
+              <PopularProductCard
+                key={product.name}
+                imgURL={product.imgURL}
+                price={product.price}
+                name={product.name}
+                rating={product.rating}
+              />
+            </div>
+            {isOpen && (
+              <ShoeCardModal
+                closeModal={closeModal}
+                imgURL={product.imgURL}
+                isOpen={isOpen}
+                name={product.name}
+                openModal={openModal}
+                price={product.price}
+                rating={product.rating}
+                setIsOpen={setIsOpen}
+              />
+            )}
+          </>
         ))}
       </div>
     </section>
